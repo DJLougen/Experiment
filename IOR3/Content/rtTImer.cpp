6c9b68d840887f115a2eaca395758a767606f85d
Daniel <99764956+DJLougen@users.noreply.github.com>
2025-07-22 19:13:01 -0400

    timer update

---
+++ IOR3/Content/rtTImer.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <chrono> // For high-resolution time
+#include <thread> // For std::this_thread::sleep_for
+#include <random> // For random number generation
+#include <limits> // For numeric_limits
